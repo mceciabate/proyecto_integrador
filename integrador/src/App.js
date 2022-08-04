916f95720551589c23727c1d94e6a7ad445cc4d0
simonlanza <simonlanza99@gmail.com>
2022-08-04 12:39:52 -0300

    issue 1

---
+++ integrador/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Home from "./pages/home/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
