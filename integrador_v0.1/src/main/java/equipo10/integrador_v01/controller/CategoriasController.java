73f4753d4b4156295c186024bbf88ea249a3d05f
Melissa <melissagarciaangel@gmail.com>
2022-08-08 11:56:14 -0500

    Modifición y adición de métodos al controller.

--- integrador_v0.1/src/main/java/equipo10/integrador_v01/controller/CategoriasController.java
+++ integrador_v0.1/src/main/java/equipo10/integrador_v01/controller/CategoriasController.java
@@ -1,12 +1,11 @@
 package equipo10.integrador_v01.controller;
 
+import equipo10.integrador_v01.model.Categorias;
 import equipo10.integrador_v01.service.ICategoriasService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/categorias")
@@ -17,8 +16,30 @@ public class CategoriasController {
 
 
     @GetMapping
+    @RequestMapping("/listar")
     public ResponseEntity<?> listarTodasCategorias(){
         categoriasService.listarTodasCategorias();
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PostMapping
+    @RequestMapping("/guardar")
+    public ResponseEntity<?> guardarCategoria(@RequestBody Categorias categoria){
+        categoriasService.guardarCategoria(categoria);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @DeleteMapping
+    @RequestMapping("/eliminar")
+    public ResponseEntity<?> eliminarCategoria(@RequestParam Long id){
+        categoriasService.eliminarCategoria(id);
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @PutMapping
+    @RequestMapping("/editar")
+    public ResponseEntity<?> actualizarCategoria(@RequestBody Categorias categoria){
+        categoriasService.actualizarCategoria(categoria);
         return ResponseEntity.ok(HttpStatus.OK);
     }
 
