2532e8333b99d881597d1dd0f49b6a50e59b4bcc
Cecilia Abate <m.cecilia.abate@gmail.com>
2022-08-09 11:12:18 -0300

    Cambios en el properties para generacion de bd

--- integrador_v0.1/src/main/java/equipo10/integrador_v01/Application.java
+++ integrador_v0.1/src/main/java/equipo10/integrador_v01/Application.java
@@ -1,8 +1,11 @@
 package equipo10.integrador_v01;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Application {
