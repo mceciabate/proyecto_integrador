4191b27542ca56028970f3b9d3b1d59db0afaf35
Cecilia Abate <m.cecilia.abate@gmail.com>
2022-08-10 12:07:43 -0300

    Excepciones

--- integrador_v0.1/src/main/java/equipo10/integrador_v01/controller/CategoriaController.java
+++ integrador_v0.1/src/main/java/equipo10/integrador_v01/controller/CategoriaController.java
@@ -3,12 +3,12 @@ import equipo10.integrador_v01.exceptions.BadRequestException;
 import equipo10.integrador_v01.exceptions.ResourceCreateException;
 import equipo10.integrador_v01.exceptions.ResourceNotFoundException;
 import equipo10.integrador_v01.model.dto.CategoriaDTO;
-import equipo10.integrador_v01.model.entity.Categoria;
 import equipo10.integrador_v01.service.ICategoriaService;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
 
 import java.util.List;
 
@@ -26,25 +26,42 @@ public class CategoriaController {
     }
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<CategoriaDTO> buscarCategoriaPorId(@PathVariable Long id) throws ResourceNotFoundException, BadRequestException {
-        categoriasService.listarCategoria();
-        return ResponseEntity.ok(categoriasService.buscarCategoriaPorId(id));
+        Optional<CategoriaDTO> categoria = Optional.ofNullable(categoriasService.buscarCategoriaPorId(id));
+        if(categoria.isPresent()){
+            return ResponseEntity.ok(categoriasService.buscarCategoriaPorId(id));
+        }
+        else throw new ResourceNotFoundException("No se encuentra el id");//Bien
+
     }
 
     @RequestMapping(value = "/guardar", method = RequestMethod.POST)
-    public ResponseEntity<CategoriaDTO> guardarCategoria(@RequestBody CategoriaDTO categoriaDTO) throws ResourceCreateException {
-        return ResponseEntity.ok(categoriasService.guardarCategoria(categoriaDTO));
+    public ResponseEntity<CategoriaDTO> guardarCategoria(@RequestBody CategoriaDTO categoriaDTO) throws BadRequestException {
+        ResponseEntity<CategoriaDTO> CategoriaDTO = null;
+        ResponseEntity<CategoriaDTO> categoria = CategoriaDTO;
+        if (categoria.hasBody()){
+            return ResponseEntity.ok(categoriasService.guardarCategoria(categoriaDTO));
+        }
+        else throw new BadRequestException("No se puede crear el recurso");//No muestra el mensaje
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<?> eliminarCategoria(@PathVariable Long id) throws ResourceNotFoundException, BadRequestException {
-        categoriasService.eliminarCategoria(id);
+    public ResponseEntity<CategoriaDTO> eliminarCategoria(@PathVariable Long id) throws ResourceNotFoundException {
+        Optional<CategoriaDTO> categoria = Optional.ofNullable(categoriasService.buscarCategoriaPorId(id));
+        if(categoria.isPresent()) {
+            categoriasService.eliminarCategoria(id);
+        }
+       else throw new ResourceNotFoundException("El id no existe");
         return null;
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public ResponseEntity<CategoriaDTO> actualizarCategoria(@PathVariable Long id, @RequestBody CategoriaDTO categoriaDTO) throws ResourceNotFoundException, BadRequestException {
-        categoriasService.actualizarCategoria(id, categoriaDTO);
-        return null;
+        Optional<CategoriaDTO> categoria = Optional.ofNullable(categoriasService.buscarCategoriaPorId(id));
+       if(categoria.isPresent()){
+           categoriasService.actualizarCategoria(id, categoriaDTO);
+           return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+       }
+       else throw new ResourceNotFoundException("El recurso que intenta actualizar no existe");
     }
 
 
