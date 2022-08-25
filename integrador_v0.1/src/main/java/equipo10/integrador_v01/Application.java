fcac5902682d1929d6f2eb51fd1976eedf6b21c1
Cecilia Abate <m.cecilia.abate@gmail.com>
2022-08-25 00:25:18 -0300

    controllers/

--- integrador_v0.1/src/main/java/equipo10/integrador_v01/Application.java
+++ integrador_v0.1/src/main/java/equipo10/integrador_v01/Application.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-@ComponentScan(basePackages = "equipo10.integrador_v01.controller")
-@ComponentScan(basePackages = "equipo10.integrador_v01.service")
+@ComponentScan(basePackages = "equipo10.integrador_v01.controller.ProductoController.java,  equipo10.integrador_v01.controller.CategoriaController.java" )
+
 
 
 public class Application {
