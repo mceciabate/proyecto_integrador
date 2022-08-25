3a4b655c6b058ce8cd0a6dcbdc88520dec140b3d
Cecilia Abate <m.cecilia.abate@gmail.com>
2022-08-25 11:43:51 -0300

    main II

--- integrador_v0.1/src/main/java/equipo10/integrador_v01/Application.java
+++ integrador_v0.1/src/main/java/equipo10/integrador_v01/Application.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-@ComponentScan(basePackages = {"equipo10.integrador_v01.controller"})
+@ComponentScan(basePackages = "equipo10.integrador_v01.controller")
 
 
 
