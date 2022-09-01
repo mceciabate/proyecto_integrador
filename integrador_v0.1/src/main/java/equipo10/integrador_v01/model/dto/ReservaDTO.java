22a0d4de94773e33cc90162806543160b4e50e7e
Melissa <melissagarciaangel@gmail.com>
2022-09-01 15:11:25 -0500

    Correción al Dto de Reservas

--- integrador_v0.1/src/main/java/equipo10/integrador_v01/model/dto/ReservaDTO.java
+++ integrador_v0.1/src/main/java/equipo10/integrador_v01/model/dto/ReservaDTO.java
@@ -1,5 +1,7 @@
 package equipo10.integrador_v01.model.dto;
 
+import equipo10.integrador_v01.model.entity.Producto;
+import equipo10.integrador_v01.model.entity.Usuario;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -14,10 +16,14 @@ public class ReservaDTO {
     private String horaInicio;
     private LocalDate fechaRecogida;
     private LocalDate fechaEntrega;
+    private Producto producto;
+    private Usuario usuario;
 
-    public ReservaDTO(String horaInicio, LocalDate fechaRecogida, LocalDate fechaEntrega) {
+    public ReservaDTO(String horaInicio, LocalDate fechaRecogida, LocalDate fechaEntrega, Producto producto, Usuario usuario) {
         this.horaInicio = horaInicio;
         this.fechaRecogida = fechaRecogida;
         this.fechaEntrega = fechaEntrega;
+        this.producto = producto;
+        this.usuario = usuario;
     }
 }
