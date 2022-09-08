6040924d0f47e1855be70e79d7d40960096d80bb
micaelagonella <gonellamicaela@gmail.com>
2022-09-07 23:17:38 -0300

    problema front 6

--- integrador_v0.1/src/main/java/equipo10/integrador_v01/config/security/SecurityConfig.java
+++ integrador_v0.1/src/main/java/equipo10/integrador_v01/config/security/SecurityConfig.java
@@ -100,7 +100,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedOrigins(Arrays.asList("http://*:8080", "*"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowCredentials(true);
         config.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
