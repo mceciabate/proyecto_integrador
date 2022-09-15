3f81e0b80b5f566010a176ca6df68852f930afa6
DaianaServin <daiana.servin@hotmail.com>
2022-09-15 18:11:06 -0300

    submit III

--- integrador/src/pages/reserve/ReserveStyles.js
+++ integrador/src/pages/reserve/ReserveStyles.js
@@ -153,8 +153,6 @@ export const FormContainer2 = styled.div`
   flex-direction: column;
   width: 100%;
 
-<<<<<<< HEAD
-=======
   h2 {
     font-weight: 700;
     font-size: 20px;
@@ -213,7 +211,6 @@ export const FormContainer2 = styled.div`
   }
 `;
 
->>>>>>> ff7c368c4b2e2d7f05e9bd7537e181727bab6218
 export const CalendarContainer = styled.div`
   height: 370px;
   width: 100%;
